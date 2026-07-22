470cff69a35415ff4f84024e8840ee4cc1bd60d7
gourijadhav08 <gourivamanjadhav06@gmail.com>
2026-07-22 22:36:24 +0530

    Add user model schema

---
+++ Backend/src/model/use.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+
+
+const userschema = new mongoose.schema({
+         username:{
+            type:String,
+            unique:[true,"This already exit"],
+            required:[true,"user name is required"]
+            },
+         email:{
+             type:String,
+             unique:[true,"This email already exit"],
+             required:[true,"Have to give emial"]
+         },
+
+            password:{
+               type:String,
+               required:[true,"password is IMP to create the account"]
+            },
+            Bio:String,
+            profileImage:{
+               type:String,
+               default:
+            }
+
+})
